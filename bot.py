53c972be04e3abfa8c2f0e0e8356e2f0699c581f
Aviatorpredictorproo <simonpeter3055@gmail.com>
2025-08-08 02:05:46 +0300

    Initial commit for Telegram Bot

--- bot.py
+++ bot.py
@@ -1,18 +1,18 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-BOT_TOKEN = '7535391262:AAG-xbokyJGmR8gHhTSpu7uI1FScmqxqYZ4'
-IMAGE_URL = 'https://i.imgur.com/OEaAePP.jpeg'
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.effective_user
-    await update.message.reply_photo(
-        photo=IMAGE_URL,
-        caption=f"👋 Hello {user.first_name}!\n\n🎉 WELCOME TO ETHIO AVIATOR PREDICTOR PRO, CLICK BELOW THE BUTTON."
-    )
-
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-
-print("🤖 Bot is running...")
-app.run_polling()
+‎from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+‎
+‎BOT_TOKEN = '7535391262:AAG-xbokyJGmR8gHhTSpu7uI1FScmqxqYZ4'
+‎IMAGE_URL = 'https://i.imgur.com/OEaAePP.jpeg'
+‎
+‎async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+‎    user = update.effective_user
+‎    await update.message.reply_photo(
+‎        photo=IMAGE_URL,
+‎        caption=f"👋 Hello {user.first_name}!\n\n🎉 WELCOME TO ETHIO AVIATOR PREDICTOR PRO, CLICK BELOW THE BUTTON."
+‎    )
+‎
+‎app = ApplicationBuilder().token(BOT_TOKEN).build()
+‎app.add_handler(CommandHandler("start", start))
+‎
+‎print("🤖 Bot is running...")
+‎app.run_polling()
